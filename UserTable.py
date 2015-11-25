b84324008ceaff9aff77a1eb0f9f7234516428ec
alon bar <alonlpc@gmail.com>
2015-11-25 20:57:23 +0200

    tmep commit

---
+++ UserTable.py
@@ -0,0 +1,28 @@
+class UserTable:
+    pass
+today:  {
+    "id":
+        {timestamp_start:"",
+         timestamp_end:"",
+         latitudeE7: "",
+         longitudeE7:"",
+         time_from_last_cooredinate:"",
+         last_cooredinates: {
+            latitudeE7:"",
+            longitudeE7:""
+         }
+
+    }
+    "id": [
+        {timestamp_start:"",
+         timestamp_end:"",
+         latitudeE7: "",
+         longitudeE7:"",
+         time_from_last_cooredinate:"",
+         last_cooredinates: {
+            latitudeE7:"",
+            longitudeE7:""
+         }
+
+}
+
