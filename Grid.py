e2ab7ce8e81111ea7e4f6d129043a645c9037186
tmrlvi <tmrlvi@gmail.com>
2015-11-25 23:26:12 +0200

    Implemented getFriendsByLocation. Maybe works

--- Grid.py
+++ Grid.py
@@ -1,7 +1,7 @@
 
 class TimeGrid(object):
     TIME_BUCKET = 10*60*1000 # 10 minutes
-    TOTAL_DAY = 24*60*60*1000
+    TIME_FRAME = 7*24*60*60*1000
     LIMIT = 60*60*1000
     def __init__(self, userData, dx, dy):
         """
@@ -23,7 +23,7 @@ class TimeGrid(object):
 
         xAmount = (gridEndX - self.gridStartX) / self.dx + 1
         yAmount = (gridEndY - self.gridStartY) / self.dy + 1
-        timeAmount = (self.endTime - self.startTime) / self.TIME_BUCKET + 1
+        timeAmount = self.TIME_FRAME / self.TIME_BUCKET + 1
         self.usersStartMatrix = [[[set()]*xAmount] * yAmount]*timeAmount
         self.usersEndMatrix = [[[set()]*xAmount] * yAmount]*timeAmount
 
@@ -51,21 +51,43 @@ class TimeGrid(object):
             # TODO: check if we need to filter by day
             self.enterUserToGrid(friendId, data["longitude"], data["latitude"], data["start_hour"], data["end_hour"])
 
-    def getFriendsByLocation(self, location, radius):
-        pass
+    def getFriendsByLocation(self, location, radius, timeRadius, byExitTime):
+        gridX = (location["longitude"] - self.gridStartX) / self.dx
+        gridY = (location["latitude"] - self.gridStartY) / self.dy
+        gridStartTime = self._normalizeTime(location["start_hour"]) / self.TIME_BUCKET
+        gridEndTime = self._normalizeTime(location["end_hour"]) / self.TIME_BUCKET
 
-    def ouputCompatibility(self, alpha):
+        friends = set()
+        for x in range(gridX - radius, gridX + radius):
+            for y in range(gridY - radius, gridY + radius):
+                for t in range(- timeRadius, timeRadius):
+                    if byExitTime:
+                        searchTime = (t + gridEndTime)%self.TIME_FRAME
+                        if searchTime < 0:
+                            searchTime += self.TIME_FRAME
+                        friends |= self.usersEndMatrix[x][y][searchTime]
+                    else:
+                        searchTime = (t + gridStartTime)%self.TIME_FRAME
+                        if searchTime < 0:
+                            searchTime += self.TIME_FRAME
+                        friends |= self.usersStartMatrix[x][y][searchTime]
+        return friends
+
+
+    def ouputCompatibility(self, alphaDistance, alphaTime):
         companions = {}
         for index in range(len(self.userData)-1):
             nextLocation = index + 1
             timeTraveled = self.userData[nextLocation]["start_time"] - self.userData[index]["end_time"]
             while timeTraveled < self.LIMIT:
-                startCompanions = self.getFriendsByLocation(self.userData[index], int(timeTraveled*alpha))
-                endCompanions = self.getFriendsByLocation(self.userData[nextLocation], 1)
+                startCompanions = self.getFriendsByLocation(self.userData[index], int(timeTraveled*alphaDistance),
+                                                            int(timeTraveled*alphaTime), True)
+                endCompanions = self.getFriendsByLocation(self.userData[nextLocation], 1, 1, False)
                 for trueCompanion in startCompanions & endCompanions:
                     companions.setdefault(trueCompanion, []).append((self.userData[index],
                                                                      self.userData[nextLocation]))
                 nextLocation += 1
+                timeTraveled = self.userData[nextLocation]["start_time"] - self.userData[index]["end_time"]
         return companions
 
 
